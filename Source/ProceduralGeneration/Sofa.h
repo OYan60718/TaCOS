9683202758ef112a0e869254330d46a900e2488d
Oliver Yan <64511896+OYan60718@users.noreply.github.com>
2024-04-08 11:17:58 +1000

    Initial commit

---
+++ Source/ProceduralGeneration/Sofa.h
@@ -0,0 +1,46 @@
+// An actor for a living room setup
+// Author: Chengyang Yan
+// Date: March 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Sofa.generated.h"
+
+UCLASS()
+class PROCEDURALGENERATION_API ASofa : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASofa();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* SofaMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* TableMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* CubeMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* BallMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* RabbitMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* UniconMesh;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	FString GetBoundingPointsOnScreen(int ResX, int ResY);
+};
